bd0170ebe97055dfa39d6e3e1e37a4d6925e1c9e
Joshua <josh@MiniStation.local>
2026-05-07 04:15:26 +0800

    fix: 修复一键启动逻辑，后台启动服务后自动打开浏览器

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
 @echo off
 cd /d "%~dp0"
-py -3 -m http.server 8080
+start /B "" py -3 -m http.server 8080
+ping -n 3 127.0.0.1 >nul
+start "" http://localhost:8080
--- start.command
+++ start.command
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-python3 -m http.server 8080
+python3 -m http.server 8080 &
+sleep 2
+open http://localhost:8080
